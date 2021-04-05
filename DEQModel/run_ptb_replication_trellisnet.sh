cb9d869e7bf5f662c4d3bd52e3c132ddd7281691
Robert Jeffrey <68836979+robertmkjeffrey@users.noreply.github.com>
2021-04-05 17:18:13 +1000

    Update run_ptb_replication_trellisnet.sh

--- DEQModel/run_ptb_replication_trellisnet.sh
+++ DEQModel/run_ptb_replication_trellisnet.sh
@@ -3,32 +3,39 @@
 if [[ $1 == 'train' ]]; then
     echo 'Run training (DEQ-TrellisNet)...'
     python train_trellisnet.py \
-        --cuda \
         --data ../data/penn/ \
         --dataset ptb \
-        --n_layer 40 \
-        --d_embed 400\
+        --n_layer 58 \
+        --d_embed 400 \
         --nhid 1000 \
         --nout 400 \
-        --clip 0.007 \
+        --epochs 500 \
+        --optim SGD \
+        --lr 20 \
+        --clip 0.225 \
+        --batch_size 16 \
+        --seq_len 110 \
+        --subseq_len 110 \
         --dropout 0.45 \
         --dropouti 0.45 \
         --wdrop 0.5 \
         --emb_dropout 0.1 \
         --dropouth 0.28 \
         --weight_decay 1.2e-6 \
-        --optim Adam \
-        --lr 1e-3 \
-        --anneal 5 \
-        --pretrain_steps 30000 \
-        --seq_len 110 \
-        --subseq_len 55 \
-        --f_thres 45 \
-        --b_thres 45 \
-        --batch_size 20 \
-        --gpu0_bsz 7 \
+        --wnorm \
+        --seed 1111 \
+        --cuda \
+        --anneal 10 \
+        --log-interval 100 \
+        --when -1 \
+        --ksize 2 \
+        --dilation 1 \
+        --n_experts 0 \
         --multi_gpu \
-        --use_gpus 0 1 2 3 \
+        --f_thresh 55 \
+        --b_thresh 80 \
+        --gpu0_bsz -1 \
+        --pretrain_steps 30000 \
         ${@:2}
 elif [[ $1 == 'eval' ]]; then
     echo 'Not supported yet'
