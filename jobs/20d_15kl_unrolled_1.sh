4064a61d5b09ed4f833d84b654c66696b65fb600
Robert Jeffrey <robert.jeffrey@anu.edu.au>
2021-05-07 17:08:24 +1000

    Added 15kl jobs

---
+++ jobs/20d_15kl_unrolled_1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Experiment Pipeline
+# Robert Jeffrey <robert.jeffrey@anu.edu.au>
+
+DEQ_DIR="${HOME}/deq/DEQModel"
+
+eval "$(/home/659/rj4139/miniconda3/bin/conda shell.bash hook)"
+conda activate honours
+
+cd ${DEQ_DIR}
+bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name 20d_15kl_unrolled_1 --n_layer 20 --pretrain_steps 15000 --seed 100 --timing --time_limit 16000
+
