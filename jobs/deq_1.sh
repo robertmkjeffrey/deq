1fd235ec04a8c22752686a441763ece782cfd2e7
Robert Jeffrey <robert.jeffrey@anu.edu.au>
2021-05-07 17:00:30 +1000

    Fixed end deadline

--- jobs/deq_1.sh
+++ jobs/deq_1.sh
@@ -8,5 +8,5 @@ eval "$(/home/659/rj4139/miniconda3/bin/conda shell.bash hook)"
 conda activate honours
 
 cd ${DEQ_DIR}
-bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name deq_1 --n_layer 40 --pretrain_steps 0 --seed 100 --timing
+bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name deq_1 --n_layer 40 --pretrain_steps 0 --seed 100 --timing --time_limit 16000
 
