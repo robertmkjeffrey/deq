911e4d76e3a9faa148b0d86bb82068df8616286f
Robert Jeffrey <robert.jeffrey@anu.edu.au>
2021-05-29 22:52:12 +1000

    Added gadi memory experiments and results

--- jobs/gadi_PTB/gpu_mem_eval.sh
+++ jobs/gadi_PTB/gpu_mem_eval.sh
@@ -1,4 +1,15 @@
-bash run_trellisnet_experiment.sh train --name mem_deq --n_layer 3 --pretrain_steps 0 --seed 9999 --timing --eval_mem
-bash run_trellisnet_experiment.sh train --name mem_3l --n_layer 3 --pretrain_steps 15000 --seed 9999 --timing --eval_mem
-bash run_trellisnet_experiment.sh train --name mem_20l --n_layer 20 --pretrain_steps 15000 --seed 9999 --timing --eval_mem
-bash run_trellisnet_experiment.sh train --name mem_40l --n_layer 40 --pretrain_steps 15000 --seed 9999 --timing --eval_mem
+#!/bin/bash
+# Experiment Pipeline
+# Robert Jeffrey <robert.jeffrey@anu.edu.au>
+
+DEQ_DIR="/g/data1a/ll21/deq/DEQModel"
+
+eval "$(/home/659/rj4139/miniconda3/bin/conda shell.bash hook)"
+conda activate honours
+
+cd ${DEQ_DIR}
+
+bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name mem_deq --n_layer 3 --pretrain_steps 0 --seed 9999 --timing --eval_mem
+bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name mem_3l --n_layer 3 --pretrain_steps 15000 --seed 9999 --timing --eval_mem
+bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name mem_20l --n_layer 20 --pretrain_steps 15000 --seed 9999 --timing --eval_mem
+bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name mem_40l --n_layer 40 --pretrain_steps 15000 --seed 9999 --timing --eval_mem
