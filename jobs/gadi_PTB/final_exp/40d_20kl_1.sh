404493b5c749b0590b876e131a5fdf03013a78a8
Robert Jeffrey <robert.jeffrey@anu.edu.au>
2021-06-03 03:09:02 +1000

    Final experiments

---
+++ jobs/gadi_PTB/final_exp/40d_20kl_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Experiment Pipeline
+# Robert Jeffrey <robert.jeffrey@anu.edu.au>
+
+DEQ_DIR="/g/data1a/ll21/deq/DEQModel"
+
+eval "$(/home/659/rj4139/miniconda3/bin/conda shell.bash hook)"
+conda activate honours
+
+cd ${DEQ_DIR}
+bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name 40d_20kl_1 --n_layer 40 --pretrain_steps 20000 --seed 100 --timing --time_limit 16000 --force-deq-validation
