34b472bb64f5757c56bc806c6f8f43f70b0ea016
Robert Jeffrey <robert.jeffrey@anu.edu.au>
2021-06-07 03:05:09 +1000

    Added convergence experiments

---
+++ jobs/gadi_PTB/final_exp/convergence_20d_20kl_1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+    # Experiment Pipeline
+    # Robert Jeffrey <robert.jeffrey@anu.edu.au>
+
+    DEQ_DIR="/g/data1a/ll21/deq/DEQModel"
+
+    eval "$(/home/659/rj4139/miniconda3/bin/conda shell.bash hook)"
+    conda activate honours
+
+    cd ${DEQ_DIR}
+    bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name convergence_20d_20kl_1 --n_layer 20 --pretrain_steps 20000 --seed 100 --epochs 82 --force-deq-validation
+    
