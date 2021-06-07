edfa6c3e0719d58171ca92247c00d02f305f62eb
Robert Jeffrey <robert.jeffrey@anu.edu.au>
2021-06-07 12:22:03 +1000

    deq experiments

---
+++ jobs/gadi_PTB/final_exp/deq_1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+    # Experiment Pipeline
+    # Robert Jeffrey <robert.jeffrey@anu.edu.au>
+
+    DEQ_DIR="/g/data1a/ll21/deq/DEQModel"
+
+    eval "$(/home/659/rj4139/miniconda3/bin/conda shell.bash hook)"
+    conda activate honours
+
+    cd ${DEQ_DIR}
+    bash ${DEQ_DIR}/run_trellisnet_experiment.sh train --name deq_1 --n_layer 3 --pretrain_steps 0 --seed 100 --timing --time_limit 16000 --force-deq-validation
+    
